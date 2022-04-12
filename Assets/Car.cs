e91afcbc2962b3fb78cf617d6acf058abe8727a2
Aneesh Babu <73874288+Aneeshbabu2106@users.noreply.github.com>
2022-04-12 14:02:37 +0530

    adding car and basic mechanics

---
+++ Assets/Car.cs
@@ -0,0 +1,31 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Car : MonoBehaviour
+{
+    float horizontalInput;
+    float verticalInput;
+
+    [SerializeField] private float moveSpeed;
+    [SerializeField] private float steerSpeed;
+
+    void Update()
+    {
+        CarMovement();
+        CarSteering();
+    }
+
+    private void CarMovement()
+    {
+        verticalInput = Input.GetAxis("Vertical");
+        transform.Translate(0,moveSpeed*verticalInput*Time.deltaTime,0);
+    }
+
+    private void CarSteering()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Rotate(0, 0, -steerSpeed*horizontalInput*Time.deltaTime);
+    }
+}
